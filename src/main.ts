b1c34eb0c5e33be83ac78fb4e5016f25edd5de0e
leandros <leandro.silva.ext@vr.com.br>
2024-09-28 15:06:49 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  await app.listen(5000);
 }
 
 bootstrap();
